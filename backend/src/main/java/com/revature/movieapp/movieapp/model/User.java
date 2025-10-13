eccfd6b3b2bc5e478a6515e9a9de2a4cbe47545e
Mohamed Abdelmagid <spareisle@gmail.com>
2025-10-13 15:48:51 -0400

    updated the user model to have basic set up

--- backend/src/main/java/com/revature/movieapp/movieapp/model/User.java
+++ backend/src/main/java/com/revature/movieapp/movieapp/model/User.java
@@ -1,5 +1,29 @@
 package com.revature.movieapp.movieapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    private String email;
+    private String username;
+    private String password;
+
+    // createdat field
+    private Long createdat;
 
 }
