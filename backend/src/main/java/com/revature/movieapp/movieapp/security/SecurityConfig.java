45f2074b927ce56ace2a130a316152ba1b949792
Syed Ahtsham <syedahtshamqau@gmail.com>
2025-11-04 18:12:48 -0500

    Updated the api.js with the s3 endpoint

--- backend/src/main/java/com/revature/movieapp/movieapp/security/SecurityConfig.java
+++ backend/src/main/java/com/revature/movieapp/movieapp/security/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173")); // React dev server ports
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://cineverse-frontend-bucket.s3-website.us-east-2.amazonaws.com")); // React dev server ports
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
